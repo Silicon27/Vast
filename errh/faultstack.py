d0e975c6b42e8b87d27db42e66e486b3650bee9a
yangmingde <yangmingde@yangjunyuandeiMac.local>
2024-07-29 20:10:43 +0200

    Added Variables, and fixed error with keywords in names beign split

--- errh/faultstack.py
+++ errh/faultstack.py
@@ -15,47 +15,24 @@ def get_characters_at_indices(strings, indices):
             characters.append(string[index])
     return characters
 
-class errgen:
-    def __init__(self, line, filepath, value, error, err_type):
-        # A list containing the lines of where the error(s) happened at
-        self.line: list = line
+def generate_error_message(line, filepath, value, error, err_type):
+    for i in range(len(line)):
+        print("\033[1;91m", end="")
+        print(f'File "{filepath[i]}", line {line[i]}')
+        print(f'    {value[i]}')
+        caret_position = ' ' * (error[i] + 4) + '^'
+        print(caret_position)
+        print(f'{get_characters_at_indices(value, error)[i]}: Error occurred here')
+        print("\033[0m", end="")
 
-        # Value of the line the error happened at
-        self.value: list = value
+    exit()
 
-        # Absolute path to the files the errors happened at
-        self.file: list = filepath
-
-        # Used to specify the character the error happened at in an index in the "value" list
-        self.error: list = error
-
-        # Specify the type of error that happened
-        self.err_type: list = err_type
-
-        # All indexes of every variable is the same, and the
-        # same index in every variable is used to reference the same error
-
-    def gen_err_msg(self):
-        for i in range(len(self.line)):
-            print("\033[1;91m", end="")
-            print(f'File "{self.file[i]}", line {self.line[i]}')
-            print(f'    {self.value[i]}')
-            caret_position = ' ' * (self.error[i] + 4) + '^'
-            print(caret_position)
-            print(f'{get_characters_at_indices(value, error)[i]}: Error occurred here')
-            print("\033[0m", end="")
-
-        exit()
-
-# Initialize the errgen class with sample data
+# Sample data
 line = [14, 17]
 filepath = ['/Users/yangmingde/PycharmProjects/Vastlang2/errh/faultstack.py', '/Users/yangmingde/PycharmProjects/Vastlang2/errh/faultstack.py']
 value = ['print("d")z', 'print("dadsa")s']
 error = [10, 14]
 err_type = ['SyntaxError', 'SyntaxError']
 
-# Create an instance of the errgen class
-error_generator = errgen(line, filepath, value, error, err_type)
-
 # Generate the error message
-error_generator.gen_err_msg()
+generate_error_message(line, filepath, value, error, err_type)
--- tokenize_lexer.py
+++ tokenize_lexer.py
@@ -11,35 +11,53 @@ class convert_to_token:
         self.keywords = keywords
         self.file = file
         self.tokens = tokens
-        self.tokens = tokens
         self.symbol = SYMBOL
 
     def tokenize(self):
         tokenized_output = []
         tokenized_dict = []
-        identifiers = []
+        tokenized_output_w_spaces = []
+        toeknized_output_w_everything = []
 
         with open(self.file, "r") as file:
             for position, line in enumerate(file, start=1):
-                # split the details on the line, whitespace not yet removed
-                temp = re.split(rf"({'|'.join(map(re.escape, self.keywords))})", line)
 
-                # Removing trailing, or leading whitespaces as well as empty strings
-                detail = [ele for ele in temp if ele.strip()]
+                # Step 1: Escape each keyword
+                escaped_keywords = list(map(re.escape, self.keywords))
 
-                # tokenize the split list
+                # Step 2: Join the escaped keywords with the pipe '|' symbol
+                joined_keywords = '|'.join(escaped_keywords)
+
+                # Step 3: Construct the regular expression pattern
+                # Match exact keywords and split on non-alphanumeric characters
+                pattern = rf"\b({joined_keywords})\b|([^\w\s])"
+
+                # Step 4: Use re.split with the constructed pattern
+                temp = re.split(pattern, line)
+
+                filtered_list = [x for x in temp if x and x.strip()]
+
+                # Append each non-empty item to the tokenized_output_w_spaces
+                tokenized_output_w_spaces.extend(filtered_list)
+                # Remove trailing, leading whitespaces, and empty strings
+                detail = [ele for ele in temp if ele and ele.strip()]
+
+                # Tokenize the split list
                 for ele in detail:
                     if ele in self.keywords:
                         tokenized_output.append(self.tokens[self.keywords.index(ele)])
+                    elif ele in self.symbol:
+                        tokenized_output.append(ele)
                     else:
                         tokenized_output.append(ele.strip())
 
-                for ele in temp:
+                for ele in detail:
                     tokenized_dict.append(
                         {"value": ele,
                          "line": position,
                          "type": "SYMBOL" if ele in self.symbol else "KEYWORD" if ele in self.keywords else "IDENTIFIER"}
                     )
+
                 # Iterate over a copy of the list to avoid modifying it while iterating
                 for item in tokenized_dict[:]:
                     if not item["value"]:
@@ -47,8 +65,4 @@ class convert_to_token:
                     elif "\n" in item["value"]:
                         item["value"] = item["value"].replace("\n", "")
 
-
-
-
-        return tokenized_output, tokenized_dict
-
+        return tokenized_output, tokenized_dict, tokenized_output_w_spaces
